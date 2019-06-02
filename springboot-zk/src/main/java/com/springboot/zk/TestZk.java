4705d4b16196db2f70181592d40d5784052027f0
changhuan.wei <527622102@qq.com>
2019-06-02 19:05:00 +0800

    add zk

---
+++ springboot-zk/src/main/java/com/springboot/zk/TestZk.java
@@ -0,0 +1,16 @@
+package com.springboot.zk;
+
+import org.apache.zookeeper.ZooKeeper;
+
+public class TestZk {
+
+
+    private static ZooKeeper zooKeeper;
+
+    static {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
