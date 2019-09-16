bfe472e2aa4859a19d99bb965a0392d6dd1573e4
wch <527622102@qq.com>
2019-09-16 10:37:00 +0800

    add code

--- springboot-rabbitmq/src/main/java/com/springboot/rabbitmq/config/RabbitConfig.java
+++ springboot-rabbitmq/src/main/java/com/springboot/rabbitmq/config/RabbitConfig.java
@@ -3,10 +3,7 @@ package com.springboot.rabbitmq.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -84,8 +81,8 @@ public class RabbitConfig implements Serializable {
      TopicExchange:多关键字匹配
      */
     @Bean
-    public DirectExchange defaultExchange() {
-        return new DirectExchange(EXCHANGE_A);
+    public TopicExchange defaultExchange() {
+        return new TopicExchange(EXCHANGE_A);
     }
 
     /**
